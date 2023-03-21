b479cd3efbaf61b3ea2b3124d8c7b4758707abec
Vasilii <pur1990@yandex.ru>
2023-03-21 23:10:46 +0300

    Добавлен код

---
+++ program/Program.cs
@@ -0,0 +1,24 @@
+﻿string[] arr1 = new string[5] {"hello", "2", "world", ":-)", "start"};
+string[] arr2 = new string[arr1.Length];
+void SecondArrayIF(string[] arr1, string[] arr2)
+{
+    int count = 0;
+    for (int i = 0; i < arr1.Length; i++)
+    {
+    if(arr1[i].Length <= 3)
+        {
+        arr2[count] = arr1[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+SecondArrayIF(arr1, arr2);
+PrintArray(arr2);
